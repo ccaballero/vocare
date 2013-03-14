ab19013566402ca70cb083e95ad5576b75ffe418
Carlos E. Caballero B <cijkb.j@gmail.com>
2013-03-14 09:13:59 -0400

    corregido un error con el helper prettyDate

--- apps/convocatorias/lib/helper/PrettyDateHelper.php
+++ apps/convocatorias/lib/helper/PrettyDateHelper.php
@@ -1,12 +1,16 @@
 <?php
 
 function pretty_date($string_date) {
-    $date = DateTime::createFromFormat('Y-m-d', $string_date);
+    if (!empty($string_date)) {
+        $date = DateTime::createFromFormat('Y-m-d', $string_date);
 
-    $months_es = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
-    $months_en = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
+        $months_es = array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre');
+        $months_en = array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December');
 
-    return $date->format('j \d\e ') .
-        $months_es[intval($date->format('n')) - 1] .
-        $date->format(' \d\e Y');
+        return $date->format('j \d\e ') .
+            $months_es[intval($date->format('n')) - 1] .
+            $date->format(' \d\e Y');
+    }
+    
+    return '';
 }
