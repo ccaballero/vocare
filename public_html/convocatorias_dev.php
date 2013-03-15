005af0664398dd6c0751fb597197b813a4379dd4
Carlos Caballero B <cijkb.j@gmail.com>
2013-03-15 12:28:49 -0400

    correcciones para el servidor en produccion

--- public_html/convocatorias_dev.php
+++ public_html/convocatorias_dev.php
@@ -2,11 +2,12 @@
 
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it or make something more sophisticated.
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1')))
+if (!in_array(@$_SERVER['REMOTE_ADDR'], array('167.157.26.129', '::1')))
 {
   die('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . '../lib' . DIRECTORY_SEPARATOR . 'symfony');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('convocatorias', 'dev', true);
--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,6 @@
 <?php
 
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . '../lib' . DIRECTORY_SEPARATOR . 'symfony');
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
 $configuration = ProjectConfiguration::getApplicationConfiguration('convocatorias', 'prod', false);
