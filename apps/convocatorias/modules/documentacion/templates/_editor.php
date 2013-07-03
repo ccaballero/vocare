66d1b8cfea776585c166bee9071d595f62a0b5cb
Carlos Caballero B <cijkb.j@gmail.com>
2013-07-03 12:34:57 -0400

    ahora si me voy

--- apps/convocatorias/modules/documentacion/templates/_editor.php
+++ apps/convocatorias/modules/documentacion/templates/_editor.php
@@ -6,24 +6,4 @@
 <script type="text/javascript">
     var tpl = <?php echo $sf_data->getRaw('tpl') ?>;
 </script>
-
-<div class="box">
-    <div class="title">
-        <span class="text">#1</span>
-        <div class="controls">
-            <ul>
-                <li><a class="shrink" href="">_</a></li>
-                <li><a class="restore" href="">o</a></li>
-                <li><a class="close" href="">x</a></li>
-            </ul>
-        </div>
-    </div>
-    <div class="content">
-        <p><label>i:</label><input type="text" /></p>
-    </div>
-</div>
-<div class="box_controls">
-    <ul>
-        <li><a class="add" href="">+</a></li>
-    </ul>
-</div>
+<script type="text/javascript" src="/js/boxes.js"></script>
---
+++ web/js/boxes.js
@@ -0,0 +1,24 @@
+// insert dream rigth here!!
+
+alert('boxes.js');
+
+//<div class="box">
+//    <div class="title">
+//        <span class="text">#1</span>
+//        <div class="controls">
+//            <ul>
+//                <li><a class="shrink" href="">_</a></li>
+//                <li><a class="restore" href="">o</a></li>
+//                <li><a class="close" href="">x</a></li>
+//            </ul>
+//        </div>
+//    </div>
+//    <div class="content">
+//        <p><label>i:</label><input type="text" /></p>
+//    </div>
+//</div>
+//<div class="box_controls">
+//    <ul>
+//        <li><a class="add" href="">+</a></li>
+//    </ul>
+//</div>
