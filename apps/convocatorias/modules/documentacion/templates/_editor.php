b81f5f449f663bd0e4204300244bdf25c7b3e227
Carlos Caballero B <cijkb.j@gmail.com>
2013-07-04 12:08:45 -0400

    trabajando en la libreria javascript para la generacion del formulario de edicion

--- apps/convocatorias/modules/documentacion/templates/_editor.php
+++ apps/convocatorias/modules/documentacion/templates/_editor.php
@@ -3,7 +3,12 @@
     <input type="text" name="volumen" value="<?php echo $object->getNombre() ?>" />
 </p>
 
-<script type="text/javascript">
-    var tpl = <?php echo $sf_data->getRaw('tpl') ?>;
-</script>
 <script type="text/javascript" src="/js/boxes.js"></script>
+<script type="text/javascript">
+    $(document).ready(function(){
+        var root=new Box(<?php echo $sf_data->getRaw('tpl') ?>)
+        root.show()
+    })
+</script>
+
+<div id="box"></div>
--- web/js/boxes.js
+++ web/js/boxes.js
@@ -1,24 +1,44 @@
-// insert dream rigth here!!
+function Box(taxonomy){
+    this.tpl_string='<p><label>{0}:</label>{1}</p>'
+    this.tpl_input='<input type="text" value="{0}" />'
+    this.tpl_box='\
+<div class="box">\
+    <div class="title">\
+        <span class="text">#1</span>\
+        <div class="controls">\
+            <ul>\
+                <li><a class="shrink" href="">_</a></li>\
+                <li><a class="restore" href="">o</a></li>\
+                <li><a class="close" href="">x</a></li>\
+            </ul>\
+        </div>\
+    </div>\
+    <div class="content">{0}</div>\
+</div>\
+<div class="box_controls">\
+    <ul><li><a class="add" href="">+</a></li></ul>\
+</div>'
 
-alert('boxes.js');
-
-//<div class="box">
-//    <div class="title">
-//        <span class="text">#1</span>
-//        <div class="controls">
-//            <ul>
-//                <li><a class="shrink" href="">_</a></li>
-//                <li><a class="restore" href="">o</a></li>
-//                <li><a class="close" href="">x</a></li>
-//            </ul>
-//        </div>
-//    </div>
-//    <div class="content">
-//        <p><label>i:</label><input type="text" /></p>
-//    </div>
-//</div>
-//<div class="box_controls">
-//    <ul>
-//        <li><a class="add" href="">+</a></li>
-//    </ul>
-//</div>
+    this.taxonomy=taxonomy
+    this.render=function(){
+        var render=''
+        for(var key in this.taxonomy){
+            var val=this.taxonomy[key]
+            switch(Object.prototype.toString.call(val)){
+                case '[object Array]':
+                    for(i=0;i<val.length;i++){
+                        box = new Box(val[i])
+                        render+=this.tpl_string.format(key,box.render())
+                    }
+                    break
+                default:
+                    input=this.tpl_input.format(val)
+                    render+=this.tpl_string.format(key,input)
+            }
+        }
+        return this.tpl_box.format(render)
+    }
+    this.show=function(){
+        $('#box').html(this.render())
+    }
+}
