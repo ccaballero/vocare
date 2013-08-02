22fa5d5fedf6017cc62839e81957a1eb5138d789
Carlos Caballero B <cijkb.j@gmail.com>
2013-08-02 12:05:27 -0400

    [TASK] Working on the tasks for import csv files

--- lib/task/vocareLoadCSVTask.class.php
+++ lib/task/vocareLoadCSVTask.class.php
@@ -4,13 +4,22 @@ class vocareLoadCSVTask extends sfBaseTask
 {
     protected function configure() {
         $this->addArguments(array(
-            new sfCommandArgument('csv_file', sfCommandArgument::REQUIRED, 'The CSV file for parsing'),
-            new sfCommandArgument('type', sfCommandArgument::REQUIRED, 'If the file is registration, reception, or habilitation'),
+            // csv file must be a absolute path, always forever and ever
+            new sfCommandArgument('csv_file',
+                sfCommandArgument::REQUIRED,
+                'The CSV file for parsing'),
+            new sfCommandArgument('type',
+                sfCommandArgument::REQUIRED,
+                'If the file is registration, reception, or habilitation'),
         ));
 
         $this->addOptions(array(
-            new sfCommandOption('application', null, sfCommandOption::PARAMETER_OPTIONAL, 'The application name', null),
-            new sfCommandOption('env', null, sfCommandOption::PARAMETER_REQUIRED, 'The environment', 'dev'),
+            new sfCommandOption('application', null,
+                sfCommandOption::PARAMETER_OPTIONAL,
+                    'The application name', null),
+            new sfCommandOption('env', null,
+                sfCommandOption::PARAMETER_REQUIRED,
+                    'The environment', 'dev'),
         ));
 
         $this->namespace        = 'vocare';
@@ -51,8 +60,36 @@ EOF;
         $this->logSection('vocare', sprintf('Create user "%s"', $arguments['username']));
     }
 
-    private function parseRegistration() {
+    private function parseRegistration($csv) {
+        $fd = fopen($csv, 'r');
 
+        // header of csv
+        fgetcsv($fd, 0, "\t");
+
+        echo '>>  convocatorias cargando información de registro .... ';
+
+        // content of csv
+        while (($csv = fgetcsv($fd, 0, ",")) !== false) {
+            // get the data
+            $last_name = $csv[0];
+            $first_name = $csv[1];
+            $email_address = $csv[2];
+            $translit = explode(' ', iconv('UTF-8', 'ASCII//TRANSLIT', $last_name));
+            $username = substr(strtolower($first_name), 0, 1) . strtolower($translit[0]);
+            $password = 'asdf';
+
+            $user = new sfGuardUser();
+            $user->setEmailAddress($email_address);
+            $user->setUsername($username);
+            $user->setPassword($password);
+            $user->setFirstName($first_name);
+            $user->setLastName($last_name);
+            $user->setIsActive(true);
+            $user->setIsSuperAdmin(false);
+            $user->save();
+        }
+        
+        fclose($fd);
     }
 
     private function parseReception() {}
