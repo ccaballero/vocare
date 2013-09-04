38d881ec0cc973b1a53e867aa66509bcc8f1ba0e
Carlos Caballero B <cijkb.j@gmail.com>
2013-09-04 12:12:25 -0400

    [POSTULANTES] Trabajando sobre los permisos para una convocatoria especifica

--- apps/convocatorias/lib/myUser.class.php
+++ apps/convocatorias/lib/myUser.class.php
@@ -3,15 +3,16 @@
 class myUser extends sfGuardSecurityUser
 {
     private $_guard;
-    
+    private $_convocatoria_permisos;
+
     public function getGuard() {
         if (!isset($this->_guard)) {
             $this->_guard = $this->getGuardUser();
         }
-        
+
         return $this->_guard;
     }
-    
+
     public function getId() {
         return $this->getGuard()->id;
     }
@@ -31,17 +32,46 @@ class myUser extends sfGuardSecurityUser
     public function getFullname() {
         return $this->getGuard()->getFullName();
     }
-    
+
     public function canView($convocatoria) {
         $state = $convocatoria->getEstado();
         $permission = 'convocatorias_view_' . strtolower($state);
         return $this->hasCredential($permission);
     }
-    
+
     public function canChangeState($convocatoria) {
         return $this->hasCredential('convocatorias_eliminar')
             || $this->hasCredential('convocatorias_promover')
             || $this->hasCredential('convocatorias_finalizar')
             || $this->hasCredential('convocatorias_anular');
+    }
+
+    public function signIn($user, $remember = false, $con = null) {
+        parent::signIn($user, $remember, $con);
+
+        // captura de permisos para una convocatoria especifica
+        $convocatorias_grupos = $user->getUsuarioGrupoConvocatoria();
+        $convocatorias = array();
+
+        foreach ($convocatorias_grupos as $convocatoria_grupo) {
+            $convocatoria = $convocatoria_grupo->getConvocatoria();
+            $grupo = $convocatoria_grupo->getGrupo();
+
+            $permisos = $grupo->getPermisos();
+            $_permisos = array();
+            foreach ($permisos as $permiso) {
+                $_permisos[] = $permiso;
+            }
+
+            if (isset($convocatorias[$convocatoria->getId()])) {
+                $convocatorias[$convocatoria->getId()][$grupo->getId()] = $_permisos;
+            } else {
+                $convocatorias[$convocatoria->getId()] = array($grupo->getId() => $_permisos);
+            }
+        }
+
+        var_dump($convocatorias);
+        die;
+        $this->_convocatoria_permisos = $convocatorias;
     }
 }
